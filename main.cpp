fbade200a5198875de51ec10413c974ad5bf154d
“Jray810” <jia.raymond19@gmail.com>
2021-12-01 14:08:39 -0500

    Added lightTrafficControl options

--- main.cpp
+++ main.cpp
@@ -10,29 +10,83 @@
  * 
  * Revision History:
  *      30NOV2021  R-11-30: Document Created, initial coding
+ *      01DEC2021  R-12-01: Added Command Line Argument Detection
  * 
  **/
 
 #include "intersect4wsl.h"
-#include "trafficController.h"
+#include "autoTrafficController.h"
+#include "lightTrafficController.h"
 #include <iostream>
 #include <thread>
 
-#define DEFAULT_SPEED_LIMIT 40
+#define DEFAULT_SPEED_LIMIT 10
+
+#define AUTO    0
+#define LIGHT   1
+#define STOP    2
+
+unsigned int controllerType;
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    // Command Line Argument Detection
+    if (argc == 1)
+    {
+        controllerType = 0;
+    }
+    if (argc == 2)
+    {
+        string typeArg = argv[1];
+        switch (typeArg)
+        {
+            case "-A":
+                controllerType = AUTO;
+                break;
+            case "-L":
+                controllerType = LIGHT;
+                break;
+            case "-S":
+                controllerType = STOP;
+                break;
+            default:
+                cerr << "Something went wrong\n";
+                return 1;
+                break;
+        }
+    }
+
     // Environment Setup Section
+
+
 
 
     // World Setup Section
     Intersect4WSL* theIntersection = new Intersect4WSL(DEFAULT_SPEED_LIMIT);
-    TrafficController* theTrafficController = new TrafficController(theIntersection);
+    TrafficController* theTrafficController;
+    switch (controllerType)
+    {
+        case AUTO:
+            theTrafficController = new AutoTrafficController(theIntersection);
+            break;
+        case LIGHT:
+            theTrafficController = new LightTrafficController(theIntersection);
+            break;
+        case STOP:
+        default:
+            theTrafficController = new AutoTrafficController(theIntersection);
+            break;
+    }
 
     // Start Operations
     theTrafficController->startController();
+    if (controllerType == LIGHT)
+    {
+        LightTrafficController* control = dynamic_cast<LightTrafficController*>(theTrafficController);
+        control->startLightCycle();
+    }
 
     // End Operations
 
--- testing.cpp
+++ testing.cpp
@@ -2,21 +2,74 @@
 #include <thread>
 #include <chrono>
 #include "intersect4wsl.h"
-#include "trafficController.h"
+#include "autoTrafficController.h"
+#include "lightTrafficController.h"
 
 #define DEFAULT_SPEED_LIMIT 1
 
 #define TEST_INTERSECTION 0
 #define TEST_ADDVEHICLES 1
 
+#define AUTO    0
+#define LIGHT   1
+#define STOP    2
+
+unsigned int controllerType;
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    // Command Line Argument Detection
+    if (argc == 1)
+    {
+        controllerType = 0;
+    }
+    if (argc == 2)
+    {
+        string typeArg = argv[1];
+        char c = typeArg[1];
+        int i_c = c - 'A';
+        switch (i_c)
+        {
+            case 'A'-'A':
+                controllerType = AUTO;
+                break;
+            case 'L'-'A':
+                controllerType = LIGHT;
+                break;
+            case 'S'-'A':
+                controllerType = STOP;
+                break;
+            default:
+                cerr << "Something went wrong\n";
+                return 1;
+                break;
+        }
+    }
+
     // Setup testing environment
     Intersect4WSL* theIntersection = new Intersect4WSL(DEFAULT_SPEED_LIMIT);
-    TrafficController* theTrafficController = new TrafficController(theIntersection);
+    TrafficController* theTrafficController;
+    switch (controllerType)
+    {
+        case 0:
+            theTrafficController = new AutoTrafficController(theIntersection);
+            break;
+        case 1:
+            theTrafficController = new LightTrafficController(theIntersection);
+            break;
+        case 2:
+        default:
+            theTrafficController = new AutoTrafficController(theIntersection);
+            break;
+    }
     theTrafficController->startController();
+    if (controllerType == LIGHT)
+    {
+        LightTrafficController* control = dynamic_cast<LightTrafficController*>(theTrafficController);
+        control->startLightCycle();
+    }
 
     // Intersection Setup Testing
     if (TEST_INTERSECTION)
@@ -56,13 +109,13 @@ int main(int argc, char *argv[])
     {
         cout << "Testing Adding Vehicles\n";
         Node* srcNode = theIntersection->getNode("0");
-        Node* destNode = theIntersection->getNode("1");
-        Vehicle* testVehicle = new Vehicle("bobby", 1, 1, 1, srcNode, destNode);
+        Node* destNode = theIntersection->getNode("3");
+        Vehicle* testVehicle = new Vehicle("bobby", 10, 10, 1, srcNode, destNode);
         theTrafficController->entryQueue.push(testVehicle);
         std::this_thread::sleep_for(std::chrono::seconds(5));
-        Vehicle* testVehicleB = new Vehicle("fred", 1, 1, 1, srcNode, destNode);
+        Vehicle* testVehicleB = new Vehicle("fred", 10, 10, 1, srcNode, destNode);
         theTrafficController->entryQueue.push(testVehicleB);
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::this_thread::sleep_for(std::chrono::seconds(50));
     }
 
     // Cleanup
