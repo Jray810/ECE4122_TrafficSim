0371597e09f2ad2beac0a26b68f5cbee7e1cc015
“Jray810” <jia.raymond19@gmail.com>
2021-11-30 22:19:34 -0500

    Added testing for adding vehicles

--- testing.cpp
+++ testing.cpp
@@ -1,45 +1,73 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "intersect4wsl.h"
+#include "trafficController.h"
 
-#define DEFAULT_SPEED_LIMIT 40
+#define DEFAULT_SPEED_LIMIT 1
+
+#define TEST_INTERSECTION 0
+#define TEST_ADDVEHICLES 1
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // Intersection Setup Testing
+    // Setup testing environment
     Intersect4WSL* theIntersection = new Intersect4WSL(DEFAULT_SPEED_LIMIT);
+    TrafficController* theTrafficController = new TrafficController(theIntersection);
+    theTrafficController->startController();
 
-    cout << theIntersection->getIntersectionID() << endl;
-    string lane_id;
-    for (int i=0; i<4; ++i)
+    // Intersection Setup Testing
+    if (TEST_INTERSECTION)
     {
-        for (int j=0; j<4; ++j)
+        cout << "Testing Intersection Setup\n";
+        cout << theIntersection->getIntersectionID() << endl;
+        string lane_id;
+        for (int i=0; i<4; ++i)
         {
-            if (i==j) {continue;}
-
-            lane_id = to_string(i) + "-" + to_string(j);
-            cout << "Lane: " << lane_id << endl;
-
-            Lane* thisLane = theIntersection->getLane(lane_id);
-
-            string testLane;
-            for (int k=0; k<4; ++k)
+            for (int j=0; j<4; ++j)
             {
-                for (int l=0; l<4; ++l)
+                if (i==j) {continue;}
+
+                lane_id = to_string(i) + "-" + to_string(j);
+                cout << "Lane: " << lane_id << endl;
+
+                Lane* thisLane = theIntersection->getLane(lane_id);
+
+                string testLane;
+                for (int k=0; k<4; ++k)
                 {
-                    testLane = to_string(k) + "-" + to_string(l);
-                    if (thisLane->isAllowedLane(testLane))
+                    for (int l=0; l<4; ++l)
                     {
-                        cout << testLane << endl;
+                        testLane = to_string(k) + "-" + to_string(l);
+                        if (thisLane->isAllowedLane(testLane))
+                        {
+                            cout << testLane << endl;
+                        }
                     }
                 }
             }
         }
     }
 
+    // Test adding vehicles
+    if (TEST_ADDVEHICLES)
+    {
+        cout << "Testing Adding Vehicles\n";
+        Node* srcNode = theIntersection->getNode("0");
+        Node* destNode = theIntersection->getNode("1");
+        Vehicle* testVehicle = new Vehicle("bobby", 1, 1, 1, srcNode, destNode);
+        theTrafficController->entryQueue.push(testVehicle);
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        Vehicle* testVehicleB = new Vehicle("fred", 1, 1, 1, srcNode, destNode);
+        theTrafficController->entryQueue.push(testVehicleB);
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
 
     // Cleanup
+    theTrafficController->stopController();
+    delete theTrafficController;
     delete theIntersection;
     return 0;
 }
