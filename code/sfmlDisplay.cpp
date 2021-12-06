529860e81323093278ccd133ddd41018e23773cb
Jray810 <jia.raymond19@gmail.com>
2021-12-06 16:43:53 -0500

    Created equations for lanes

--- code/sfmlDisplay.cpp
+++ code/sfmlDisplay.cpp
@@ -15,6 +15,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
 #include "intersect4wsl.h"
 #include "autoTrafficController.h"
 #include "lightTrafficController.h"
@@ -41,8 +42,20 @@ sf::Font boldFont;
 // Global Variables
 Intersect4WSL* theIntersection;
 TrafficController* theTrafficController;
+std::map<std::string, Vehicle*> VehicleCollection;
+
+struct Coord
+{
+    double x;
+    double y;
+    double r;
+};
 
 void cleanup();
+Coord getPos(std::string lane_id, double t);
+Coord getPosRight(unsigned int lane, double t);
+Coord getPosStraight(unsigned int lane, double t);
+Coord getPosLeft(unsigned int lane, double t);
 
 using namespace sf;
 
@@ -96,7 +109,7 @@ int main(int argc, char *argv[])
         titleTextRect.width / 2.0f,
         titleTextRect.top +
         titleTextRect.height / 2.0f);
-    titleText.setPosition(WINDOW_XDIM / 2.0f, WINDOW_YDIM / 5.0f);
+    titleText.setPosition(WINDOW_XDIM / 4.0f, WINDOW_YDIM / 6.0f);
 
     // Create and Initialize Background Sprite
     Texture textureBackground;
@@ -127,6 +140,18 @@ int main(int argc, char *argv[])
     spriteVehicleD.setTexture(textureVehicleD);
     spriteVehicleD.setPosition(160,40);
 
+    // Adjust Scaling
+    spriteVehicleA.setScale(Vector2f(0.3f, 0.3f));
+    spriteVehicleB.setScale(Vector2f(0.3f, 0.3f));
+    spriteVehicleC.setScale(Vector2f(0.3f, 0.3f));
+    spriteVehicleD.setScale(Vector2f(0.3f, 0.3f));
+
+    // Set Origin
+    spriteVehicleA.setOrigin(30, 59);
+    spriteVehicleB.setOrigin(30, 59);
+    spriteVehicleC.setOrigin(30, 59);
+    spriteVehicleD.setOrigin(30, 59);
+
     // Setup Environment
     theIntersection = new Intersect4WSL(DEFAULT_SPEED_LIMIT);
     switch (controllerType)
@@ -156,7 +181,11 @@ int main(int argc, char *argv[])
      **/
     window.clear();
     window.draw(spriteBackground);
+    window.draw(titleText);
+    window.display();
 
+    double t = 0;
+    double s = 8;
 
     // Enter Display Loop
     while (window.isOpen())
@@ -177,12 +206,30 @@ int main(int argc, char *argv[])
         window.clear();
         // Draw Background
         window.draw(spriteBackground);
+        Coord posA = getPos("0-1", t);
+        Coord posB = getPos("0-2", t);
+        Coord posC = getPos("0-3", t);
+        Coord posD = getPos("2-0", t);
+        spriteVehicleA.setPosition(WINDOW_XDIM/2 + posA.x*s, WINDOW_YDIM/2 + posA.y*s);
+        spriteVehicleB.setPosition(WINDOW_XDIM/2 + posB.x*s, WINDOW_YDIM/2 + posB.y*s);
+        spriteVehicleC.setPosition(WINDOW_XDIM/2 + posC.x*s, WINDOW_YDIM/2 + posC.y*s);
+        spriteVehicleD.setPosition(WINDOW_XDIM/2 + posD.x*s, WINDOW_YDIM/2 + posD.y*s);
+        spriteVehicleA.setRotation(posA.r);
+        spriteVehicleB.setRotation(posB.r);
+        spriteVehicleC.setRotation(posC.r);
+        spriteVehicleD.setRotation(posD.r);
         window.draw(spriteVehicleA);
+        window.draw(spriteVehicleB);
+        window.draw(spriteVehicleC);
+        window.draw(spriteVehicleD);
 
         // Draw Text
         window.draw(titleText);
         // Display Window
         window.display();
+
+        t += 0.1;
+        if (t > 100) {t = 0;}
     }
 
     // Cleanup
@@ -199,5 +246,224 @@ void cleanup()
 
 void vehicleSpawner()
 {
+    // Spawn one vehicle
+    std::string randomizedID;
+    unsigned int src;
+    unsigned int dest;
+    Vehicle* newVehicle = new Vehicle(randomizedID, 10, 10, 1, theIntersection->getNode(std::to_string(src)), theIntersection->getNode(std::to_string(dest)));
+    VehicleCollection.insert({newVehicle->getVehicleID(), newVehicle});
+}
+
+// Gives offset from center of intersection
+Coord getPos(std::string lane_id, double t)
+{
+    Coord position;
+
+    if (lane_id == "0-1" || lane_id == "1-2" || lane_id == "2-3" || lane_id == "3-0")
+    {
+        unsigned int src = lane_id[0]-'0';
+        position = getPosRight(src, t);
+    }
+    else if (lane_id == "0-2" || lane_id == "1-3" || lane_id == "2-0" || lane_id == "3-1")
+    {
+        unsigned int src = lane_id[0]-'0';
+        position = getPosStraight(src, t);
+    }
+    else
+    {
+        unsigned int src = lane_id[0]-'0';
+        position = getPosLeft(src, t);
+    }
+
+    return position;
+}
+
+// Right Turn Position Calculator
+Coord getPosRight(unsigned int lane, double t)
+{
+    Coord position;
+
+    if (t < 40)
+    {
+        position.x = -1 * (40-t);
+        position.y = -1 * 7;
+    }
+    else if (t <= 60)
+    {
+        position.x = 7 * sin((t-40)*M_PI/40);
+        position.y = -1 * 7 * cos((t-40)*M_PI/40);
+    }
+    else
+    {
+        position.x = 7;
+        position.y = t-60;
+    }
+
+    double tmp;
+    switch(lane)
+    {
+        case 0:
+            position.x -= 10;
+            position.y += 10;
+            position.r = 0;
+            break;
+        case 1:
+            position.x *= -1;
+            tmp = position.x;
+            position.x = position.y;
+            position.y = tmp;
+            position.x += 10;
+            position.y += 10;
+            position.r = -90;
+            break;
+        case 2:
+            position.x *= -1;
+            position.y *= -1;
+            position.x += 10;
+            position.y -= 10;
+            position.r = 180;
+            break;
+        case 3:
+            position.y *= -1;
+            tmp = position.x;
+            position.x = position.y;
+            position.y = tmp;
+            position.x -= 10;
+            position.y -= 10;
+            position.r = 90;
+            break;
+        default:
+            std::cerr << "something went wrong!\n";
+            break;
+    }
     
+    position.r += 90;
+
+    if (t > 60)
+    {
+        position.r += 90;
+        if (position.r > 180)
+        {
+            position.r -= 360;
+        }
+    }
+    else if (t >= 40)
+    {
+        std::cout << "shiet\n";
+    }
+
+    return position;
+}
+
+// Straight Position Calculator
+Coord getPosStraight(unsigned int lane, double t)
+{
+    Coord position;
+
+    double a = t-50;
+    double b = 3;
+
+    switch (lane)
+    {
+        case 0:
+            position.x = a;
+            position.y = b;
+            position.r = 90;
+            break;
+        case 2:
+            position.x = -1*a;
+            position.y = -1*b;
+            position.r = -90;
+            break;
+        case 1:
+            position.x = b;
+            position.y = -1*a;
+            position.r = 0;
+            break;
+        case 3:
+            position.x = -1*b;
+            position.y = a;
+            position.r = 180;
+            break;
+        default:
+            std::cerr << "something went wrong!\n";
+            break;
+    }
+
+    return position;
+}
+
+// Left Position Calculator
+Coord getPosLeft(unsigned int lane, double t)
+{
+    Coord position;
+
+    if (t < 40)
+    {
+        position.x = -1 * (40-t);
+        position.y = 13;
+    }
+    else if (t <= 60)
+    {
+        position.x = 13 * sin((t-40)*M_PI/40);
+        position.y = 13 * cos((t-40)*M_PI/40);
+    }
+    else
+    {
+        position.x = 13;
+        position.y = 60-t;
+    }
+
+    double tmp;
+    switch(lane)
+    {
+        case 0:
+            position.x -= 10;
+            position.y -= 10;
+            position.r = 90;
+            break;
+        case 1:
+            position.x *= -1;
+            tmp = position.x;
+            position.x = position.y;
+            position.y = tmp;
+            position.x -= 10;
+            position.y += 10;
+            position.r = 0;
+            break;
+        case 2:
+            position.x *= -1;
+            position.y *= -1;
+            position.x += 10;
+            position.y += 10;
+            position.r = -90;
+            break;
+        case 3:
+            position.y *= -1;
+            tmp = position.x;
+            position.x = position.y;
+            position.y = tmp;
+            position.x += 10;
+            position.y -= 10;
+            position.r = 180;
+            break;
+        default:
+            std::cerr << "something went wrong!\n";
+            break;
+    }
+
+    if (t > 60)
+    {
+        position.r -= 90;
+        if (position.r < -180)
+        {
+            position.r += 360;
+        }
+    }
+    else if (t >= 40)
+    {
+        std::cout << "shiet\n";
+    }
+
+    return position;
 }
