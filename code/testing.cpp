6edacc8ef41bf952d0cf049ebb887483f25919a1
Jray810 <jia.raymond19@gmail.com>
2021-12-07 16:35:01 -0500

    Debugging and code cleanup for testing.cpp

--- code/testing.cpp
+++ code/testing.cpp
@@ -24,13 +24,16 @@
 #include "lightTrafficController.h"
 #include "stopTrafficController.h"
 
+// Default Speed Limit
 #define DEFAULT_SPEED_LIMIT 4
 
+// Tests (Only one of them should be on at a time)
 #define TEST_INTERSECTION 0
 #define TEST_ADDVEHICLES 0
 #define TEST_STOPCONTROLLER 0
 #define TEST_TRAFFICJAM 1
 
+// Traffic Controller Type
 #define AUTO    0
 #define LIGHT   1
 #define STOP    2
@@ -39,56 +42,119 @@ unsigned int controllerType;
 // Default Speed Limit
 #define DEFAULT_SPEED_LIMIT 4
 
-// Default Tick Speed
-#define DEFAULT_TICK_MICROS 1000000
+// Default Tick Speed in microseconds
+#define DEFAULT_TICK_MICROS 100000
+unsigned int tickSpeed;
 
-using namespace std;
+// Vehicle Spawn Chance per Iteration
+#define DEFAULT_PROB 25
+unsigned int probability;
 
+// Global Variables
+Intersect4WSL* theIntersection;
+TrafficController* theTrafficController;
+std::vector<Vehicle*> vehicleCollection;
+
+// Let's run some tests!
 int main(int argc, char *argv[])
 {
     // Set debug to true
     DEBUG = true;
 
-    // Seed RNG
-    srand(time(NULL));
-
-    // Command Line Argument Detection
+    /**
+     * Command Line Argument Processing
+     **/
+    controllerType = AUTO;
     tickSpeed = DEFAULT_TICK_MICROS;
-    if (argc == 1)
+    probability = DEFAULT_PROB;
+    // Check for second command line argument (--help or Controller Type)
+    if (argc >= 2)
     {
-        controllerType = 0;
-    }
-    if (argc == 2)
-    {
-        string typeArg = argv[1];
+        std::string typeArg = argv[1];
+        // Check Number of Characters
+        if (typeArg.length() != 2)
+        {
+            std::cerr << "Invalid Second Arg!\n"
+                      << "Received " << typeArg << std::endl;
+            return 1;
+        }
+        // Controller Type Flag
+        if (typeArg[0] != '-')
+        {
+            std::cerr << "Invalid Signature!\n"
+                      << "Received" << typeArg[0] << std::endl;
+            return 1;
+        }
         char c = typeArg[1];
         int i_c = c - 'A';
+        // Check Controller Flag Type
         switch (i_c)
         {
             case 'A'-'A':
                 controllerType = AUTO;
                 break;
             case 'L'-'A':
-                controllerType = LIGHT;
+                controllerType = LIGHT; // Will probably not work properly :(
                 break;
             case 'S'-'A':
                 controllerType = STOP;
                 break;
             default:
-                cerr << "Something went wrong\n";
+                std::cerr << "Invalid Command Line Input!\n"
+                          << "Received " << typeArg << std::endl;
                 return 1;
                 break;
         }
     }
+    // Check for third command line argument (Tick Speed)
     if (argc >= 3)
     {
         std::string typeArg = argv[2];
+        // Check for positive value
+        for (int i=0; i<typeArg.length(); ++i)
+        {
+            if (!isdigit(typeArg[i]))
+            {
+                std::cerr << "Invalid Command Line Input!\n"
+                          << "Found non-digit" << typeArg[i] << std::endl;
+                return 1;
+            }
+        }
         tickSpeed = stoi(typeArg);
     }
+    // Check for fourth command line argument (Spawn Probability)
+    if (argc >= 4)
+    {
+        std::string typeArg = argv[3];
+        // Check for positive value
+        for (int i=0; i<typeArg.length(); ++i)
+        {
+            if (!isdigit(typeArg[i]))
+            {
+                std::cerr << "Invalid Command Line Input!\n"
+                          << "Found non-digit" << typeArg[i] << std::endl;
+                return 1;
+            }
+        }
+        probability = stoi(typeArg);
+    }
+    // Check for too many args
+    if (argc > 4)
+    {
+        std::cerr << "Too many arguments!\n"
+                    << "Received " << argc << ", support only offer up to 4.\n"
+                    << "Run --help for more info.\n"
+                    << "Quitting...\n";
+        return 1;
+    }
 
-
-    // Setup testing environment
-    double speedLimit = controllerType == STOP ? 1 : DEFAULT_SPEED_LIMIT;
+    /**
+     * Environment Setup
+     **/
+    srand(time(NULL));
+    double speedLimit = DEFAULT_SPEED_LIMIT;
+    // For scaling the dimensions of the intersection on a screen
+    double scale = controllerType == AUTO ? 40 : 36;
     theIntersection = new Intersect4WSL(speedLimit);
     switch (controllerType)
     {
@@ -105,7 +171,9 @@ int main(int argc, char *argv[])
             theTrafficController = new AutoTrafficController(theIntersection, tickSpeed);
             break;
     }
+    // Start the Controller
     theTrafficController->startController();
+    // Start traffic lights thread if a traffic light controller
     if (controllerType == LIGHT)
     {
         LightTrafficController* control = dynamic_cast<LightTrafficController*>(theTrafficController);
@@ -115,29 +183,29 @@ int main(int argc, char *argv[])
     // Intersection Setup Testing
     if (TEST_INTERSECTION)
     {
-        cout << "Testing Intersection Setup\n";
-        cout << theIntersection->getIntersectionID() << endl;
-        string lane_id;
+        std::cout << "Testing Intersection Setup\n";
+        std::cout << theIntersection->getIntersectionID() <<  std::endl;
+        std::string lane_id;
         for (int i=0; i<4; ++i)
         {
             for (int j=0; j<4; ++j)
             {
                 if (i==j) {continue;}
 
-                lane_id = to_string(i) + "-" + to_string(j);
-                cout << "Lane: " << lane_id << endl;
+                lane_id = std::to_string(i) + "-" + std::to_string(j);
+                std::cout << "Lane: " << lane_id <<  std::endl;
 
                 Lane* thisLane = theIntersection->getLane(lane_id);
 
-                string testLane;
+                std::string testLane;
                 for (int k=0; k<4; ++k)
                 {
                     for (int l=0; l<4; ++l)
                     {
-                        testLane = to_string(k) + "-" + to_string(l);
+                        testLane = std::to_string(k) + "-" + std::to_string(l);
                         if (thisLane->isAllowedLane(testLane))
                         {
-                            cout << testLane << endl;
+                            std::cout << testLane <<  std::endl;
                         }
                     }
                 }
@@ -148,13 +216,19 @@ int main(int argc, char *argv[])
     // Test adding vehicles
     if (TEST_ADDVEHICLES)
     {
-        cout << "Testing Adding Vehicles\n";
+        std::cout << "Testing Adding Vehicles\n";
         Vehicle* testVehicleA = new Vehicle("Alice", 10, 10, 1, theIntersection->getNode("0"), theIntersection->getNode("2"));
+        vehicleCollection.push_back(testVehicleA);
+        std::cout << "Vehicle pushed to queue\n";
         theTrafficController->entryQueue.push(testVehicleA);
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
         Vehicle* testVehicleB = new Vehicle("Bob", 10, 10, 1, theIntersection->getNode("1"), theIntersection->getNode("3"));
+        vehicleCollection.push_back(testVehicleB);
+        std::cout << "Vehicle pushed to queue\n";
         theTrafficController->entryQueue.push(testVehicleB);
         Vehicle* testVehicleC = new Vehicle("Carol", 10, 10, 1, theIntersection->getNode("1"), theIntersection->getNode("2"));
+        vehicleCollection.push_back(testVehicleC);
+        std::cout << "Vehicle pushed to queue\n";
         theTrafficController->entryQueue.push(testVehicleC);
         std::this_thread::sleep_for(std::chrono::seconds(30));
     }
@@ -162,12 +236,18 @@ int main(int argc, char *argv[])
     // Test Stop Sign Controlled Traffic Controller
     if (TEST_STOPCONTROLLER)
     {
-        cout << "Testing Stop Sign Controlled Traffic Controller\n";
+        std::cout << "Testing Stop Sign Controlled Traffic Controller\n";
         Vehicle* testVehicleA = new Vehicle("Alice", 10, 10, 1, theIntersection->getNode("0"), theIntersection->getNode("2"));
+        vehicleCollection.push_back(testVehicleA);
+        std::cout << "Vehicle pushed to queue\n";
         theTrafficController->entryQueue.push(testVehicleA);
         Vehicle* testVehicleB = new Vehicle("Bob", 10, 10, 1, theIntersection->getNode("1"), theIntersection->getNode("3"));
+        vehicleCollection.push_back(testVehicleB);
+        std::cout << "Vehicle pushed to queue\n";
         theTrafficController->entryQueue.push(testVehicleB);
         Vehicle* testVehicleC = new Vehicle("Carol", 10, 10, 1, theIntersection->getNode("1"), theIntersection->getNode("2"));
+        vehicleCollection.push_back(testVehicleC);
+        std::cout << "Vehicle pushed to queue\n";
         theTrafficController->entryQueue.push(testVehicleC);
         std::this_thread::sleep_for(std::chrono::seconds(30));
     }
@@ -175,12 +255,14 @@ int main(int argc, char *argv[])
     // Test Traffic Jame
     if (TEST_TRAFFICJAM)
     {
-        cout << "Testing Traffic Jam\n";
+        std::cout << "Testing Traffic Jam\n";
         for (int i=0; i<10; ++i)
         {
             int dest = (rand() % 2) + 2;
             int l = rand() % 2;
-            Vehicle* testVehicle = new Vehicle(std::to_string(i) + std::to_string(l), 10, 10, 1, theIntersection->getNode(std::to_string(l)), theIntersection->getNode(std::to_string(dest)));
+            Vehicle* testVehicle = new Vehicle(std::to_string(i) + std::to_string(l), 10, 10, 1, theIntersection->getNode(std::to_string(l)), theIntersection->getNode(std::to_string(dest)));    
+            std::cout << "Vehicle pushed to queue\n";
+            vehicleCollection.push_back(testVehicle);
             theTrafficController->entryQueue.push(testVehicle);
         }
         std::this_thread::sleep_for(std::chrono::seconds(30));
@@ -190,5 +272,10 @@ int main(int argc, char *argv[])
     theTrafficController->stopController();
     delete theTrafficController;
     delete theIntersection;
+    for (int i=0; i<vehicleCollection.size(); ++i)
+    {
+        delete vehicleCollection[i];
+    }
+    vehicleCollection.clear();
     return 0;
 }
